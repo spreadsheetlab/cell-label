d0d7c2a80b81aa6ca5df89d7564f709df4bc5b3d
Efthimia Aivaloglou <e.aivaloglou@tudelft.nl>
2014-05-06 10:51:05 +0200

    Send data to get.aspx using jQuery post

--- xlGame/get.aspx.cs
+++ xlGame/get.aspx.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string spreadsheet = Request.Form[0];
+        string spToken = Request.Form[1];
+        string cell = Request.Form[2];
+        ISet<String> labels = new HashSet<String>();
 
+        for (int i = 3; i < Request.Form.Count; i++)
+        {
+            labels.Add(Request.Form[i]);
+        }
     }
 }
