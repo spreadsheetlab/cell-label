438dd55bc59f47f37f98d4cc3228b731af785251
Efthimia Aivaloglou <e.aivaloglou@tudelft.nl>
2014-05-20 14:13:53 +0200

    First version of tool for preparing the Excels for the xlGame

---
+++ xlPrep/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace xlPrep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Resolves to C:\Users\<username>\AppData\Roaming\xls
+            //var filesPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "xls");
+            var filesPath = "C:\\Euses";
+            var outputPath = "C:\\EusesOutput";
+
+            XlTransform t = new XlTransform();
+            t.Transform(filesPath, outputPath);
+        }
+    }
+}
---
+++ xlPrep/XlTransform.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using GemBox.Spreadsheet;
+using Infotron.PerfectXL;
+using System.IO;
+
+namespace xlPrep
+{
+    class XlTransform
+    {
+        //TODO on spredsheets: 
+        //- Seperate worksheets in different files
+        //- Add hidden worksheet named hidden
+        //- Add the 2 conditional formating rules
+
+        public void Transform(String inputPath, String outputPath)
+        {
+            var excelReader = new ExcelReader();
+            var excelWriter = new ExcelWriter();
+            int i = 0;
+            try
+            {
+                foreach (var file in Directory.EnumerateFiles(inputPath, "*.xls*", SearchOption.AllDirectories))
+                {
+                    try
+                    {
+                        System.Diagnostics.Debug.WriteLine("Processing " + file);
+                        i++;
+                        if (i > 50)
+                        {
+                            return;
+                        }
+                        excelReader.Read(file);
+                        var xls = excelReader.GemboxExcel;
+
+
+                        xls.Save(Path.Combine(outputPath, Path.GetFileNameWithoutExtension(file) + ".xlsx"), SaveOptions.XlsxDefault);
+                    }
+                    catch (Exception e)
+                    {   //continue to the next file
+                        System.Diagnostics.Debug.WriteLine("Error processing " + file + ": " + e.Message + e.InnerException);
+                    }
+                }
+            }
+            catch (DirectoryNotFoundException e)
+            {
+                System.Diagnostics.Debug.WriteLine(e.Message + e.InnerException);
+            }
+            System.Diagnostics.Debug.WriteLine("Analyzed " + i + " files.");
+        }
+        
+    }
+}
