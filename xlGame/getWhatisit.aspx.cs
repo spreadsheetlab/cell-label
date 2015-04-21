8a6a691d9714811c3f47efa47d220afa5c9b3335
Efthimia Aivaloglou <e.aivaloglou@tudelft.nl>
2015-04-21 14:34:21 +0200

    Updated js with input controls functionality

--- xlGame/getWhatisit.aspx.cs
+++ xlGame/getWhatisit.aspx.cs
@@ -6,7 +6,7 @@ public partial class _Default : System.Web.UI.Page
 {
     private const String oneDriveFileTokenPrefix = "SD";
     private const String oneDriveFileTokenSuffix = "/517479313637659748/t=0&s=0";
-    private const int prefixFieldsNo = 5;
+    private const int prefixFieldsNo = 4;
 
     protected void Page_Load(object sender, EventArgs e)
     {
@@ -23,16 +23,14 @@ public partial class _Default : System.Web.UI.Page
     private void saveData(){
         string spreadsheet = Request.Form[0];
         string xlsToken = Request.Form[1];
-        string cell = Request.Form[2];
-        string skipExpl = "\"" + removeBreakChars(Request.Form[3]) + "\"";
-        string userEmail = removeBreakChars(Request.Form[4]);
+        string skipExpl = "\"" + removeBreakChars(Request.Form[2]) + "\"";
+        string userEmail = removeBreakChars(Request.Form[3]);
         string clientIP = getClientIPAddress();
         string linePrefix = DateTime.Now + "\t"
                 + clientIP + "\t"
                 + "email:" + userEmail + "\t"
                 + xlsToken + "\t"
                 + spreadsheet + "\t"
-                + cell + "\t"
                 + "skip:" + skipExpl;
 
         StreamWriter sw = File.AppendText(Request.PhysicalApplicationPath + @"\whatisitData\results.txt");
@@ -149,15 +147,16 @@ public partial class _Default : System.Web.UI.Page
         Response.Write("{ \"xls\": \"" + xls + "\" ,");
     }
 
-    private String getNextXlsToken() 
+    private String getNextXlsToken()  
     {
         var xlsFiles = File.ReadAllLines(Request.PhysicalApplicationPath + @"\whatisitData\input.txt");
         var randomXls = new Random().Next(0, xlsFiles.Length);
         var line = xlsFiles[randomXls];
 
-        //from line="myTest.xlsx#file.072e74b1abfc5464.72E74B1ABFC5464!197"
-        //return spToken = "SD72E74B1ABFC5464!197/517479313637659748/t=0&s=0";
-        return oneDriveFileTokenPrefix + line.Split('#')[1].Split('.')[2] + oneDriveFileTokenSuffix;
+        //from line="chris_germany__1813__strg_proxy.xlsx#file.072e74b1abfc5464.72E74B1ABFC5464!197"
+        //return spToken = "strg_proxy.xlsx#SD72E74B1ABFC5464!197/517479313637659748/t=0&s=0";
+        var enronFileName = line.Substring(line.LastIndexOf("__") + 2).Split('#')[0];
+        return enronFileName + "#" + oneDriveFileTokenPrefix + line.Split('#')[1].Split('.')[2] + oneDriveFileTokenSuffix;
     }
 
     private string getClientIPAddress()
--- xlGame/whatisit.js
+++ xlGame/whatisit.js
@@ -1,9 +1,6 @@
 ﻿var xlsToken = null; //the SkyDrive filetoken string
 var ewa = null; //the Excel Services Web Part
 var workbook = null //the active workbook
-var questionCell = null; //the cell to be labeled
-var labels = []; //list with the labels the user has clicked, each represented as a tuple eg "C3, value"
-var remainingCells; //the number of cells to be labeled before changing workbook
 var useremail = "";
 var keepUpdated = false;
 var userScore = 0;
@@ -33,15 +30,14 @@ $('#welcomeModal').modal({
 })
 
 function initCollectors() {
-    selectQuestionCell();
-    labels = [];
-    //de-color cells
-    workbook.getRangeA1Async("hidden!A2", setHiddenValue, "");
-    showSkipText();
+    $('#result').val = "-1";
+    resultChanged();
+    $("#questionHeader").html("Of what type is workbook <span class=\"label label-warning\">" + xlsToken.split("#")[0] + "</span>?");
+    $("#questionExp").show();
 }
 
 function loadEwaOnPageLoad() {
-    postData({}, true);
+    postData({});
 }
 
 function btnNextClick() {
@@ -50,27 +46,19 @@ function btnNextClick() {
     var data = {
         spreadsheet: workbook.getWorkbookPath(),
         xlsToken: xlsToken,
-        cell: questionCell.getAddressA1(),
-        skipExpl: $("#txtSkip").val(),
+        skipExpl: $("#txtOther").val(),
         userEmail: useremail + "#" + keepUpdated,
-        labels: labels
+        labels: $('#result').val()
     };
 
-    $("#questionHeader").html("What describes&hellip;");
+    $("#questionHeader").html("Loading&hellip;");
     $("#questionExp").hide();
-    remainingCells--;
-    if (remainingCells < 0) {
-        //Change worksheet
-        postData(data, true);
-    } else {
-        //Change question cell in the same worksheet
-        postData(data, false);
-        initCollectors();
-    }
+    //Change worksheet
+    postData(data);
 }
 
 function updateSmiley() {
-    (labels.length > 0 || $("#txtSkip").val().length > 0) ? smileyScore++ : smileyScore--;
+    ($('#result').val != "-1" || $("#txtOther").val().length > 0) ? smileyScore++ : smileyScore--;
 
     var evenScore = Math.ceil(smileyScore / 2.0) * 2;
     if (evenScore >= -6 && evenScore <= 12) {
@@ -78,15 +66,12 @@ function updateSmiley() {
     }
 }
 
-function postData(data, changeXls) {
+function postData(data) {
     posting = $.post("getWhatIsIt.aspx", data);
 
     posting.done(function (resp) {
-        if (changeXls) {
-            xlsToken = resp.xls;
-            loadExcel(xlsToken);
-            remainingCells = Math.floor(Math.random() * 3) + 2; //number of tries before worksheet change, 2 to 2+(3-1) = 4
-        }
+        xlsToken = resp.xls;
+        loadExcel(xlsToken);
         $("#statsDay").html(resp.statsDay);
         $("#statsWeek").html(resp.statsWeek);
         $("#statsMonth").html(resp.statsMonth);
@@ -105,122 +90,30 @@ function loadExcel(token) {
         interactivityOptions: {
             allowTypingAndFormulaEntry: false,
             allowParameterModification: false,
-            allowSorting: false,
-            allowFiltering: false,
-            allowPivotTableInteractivity: false
+            allowHyperlinkNavigation: false
         }
     };
 
     $("#excelDiv").remove();
     $("#excelDivParent").append("<div id=\"excelDiv\" style=\"width: 1000px; height: 600px\"></div>");
 
-    Ewa.EwaControl.loadEwaAsync(token, "excelDiv", props, onExcelLoaded);
+    Ewa.EwaControl.loadEwaAsync(token.split("#")[1], "excelDiv", props, onExcelLoaded);
 }
 
 function onExcelLoaded(result) {
     ewa = Ewa.EwaControl.getInstances().getItem(Ewa.EwaControl.getInstances().getCount() - 1);
     workbook = ewa.getActiveWorkbook();
-    ewa.add_activeCellChanged(onCellSelectionChange);
     initCollectors();
 }
 
-function selectQuestionCell() {
-    var qCell = randomCell();
-    questionCell = workbook.getRange(workbook.getActiveSheet().getName(), qCell.row, qCell.column, 1, 1);
-    questionCell.getValuesAsync(1, checkNonEmpty, null);
-}
+function updateLabels(asyncResult) { //FENIA! htan sto onCellSelectionChange
 
-function checkNonEmpty(asyncResult) {
-    var value = asyncResult.getReturnValue()[0][0];
-    if (value == "") {
-        //select another cell
-        selectQuestionCell();
-    } else {
-        //set value to hidden!A1 to make confitional formatting color the ewaCell
-        var qCellContent = asyncResult.getReturnValue()[0][0];
-        if (qCellContent.length > 35) {
-            qCellContent = qCellContent.substring(0, 30) + "&hellip;";
-        }
-        workbook.getRangeA1Async("hidden!A1", setHiddenValue, questionCell.getAddressA1() + ",");
-        $("#questionHeader").html("What describes <span class=\"label label-warning\">" + qCellContent + "</span> in " + questionCell.getAddressA1().split("!")[1] + "?");
-        $("#questionExp").show();
-    }
-}
-
-// Handle the active cell changed event.
-function onCellSelectionChange(rangeArgs) {
-    var cell = workbook.getActiveCell();
-    cell.getValuesAsync(1, updateLabels, cell);
-}
-
-function updateLabels(asyncResult) {
-    var cellValue = asyncResult.getReturnValue()[0][0];
-    if (cellValue == "") {
-        return;
-    }
-
-    var cellAddr = asyncResult.getUserContext().getAddressA1().split("!")[1];
-    var index = indexOfLabel(cellAddr);
-    if (index >= 0) { //Cell already in list, de-selected -> remove from labels
-        labels.splice(index, 1);
-        userScore--;
-    }
-    else { //Cell selected -> add to labels
-        labels[labels.length] = [cellAddr, "\"" + cellValue + "\""];
         userScore++;
-    }
-    updateLabelColors();
+    
     $("#noOfLabels").html(userScore);
-
-    labels.length > 0 ? hideSkipText() : showSkipText();
 }
 
-function showSkipText() {
-    $("#results").html("");
-    $("#txtSkip").val("");
-    $("#txtSkip").show();
-    $("#lblSkip").text("Skip challenge because ");
-}
-
-function hideSkipText() {
-    $("#results").html("It is <span class=\"label label-success\">" + getLabelsStr(1, "</span> <span class=\"label label-success\">") + "</span>");
-    $("#txtSkip").hide();
-    $("#lblSkip").text("");
-}
-
-function indexOfLabel(cellAddr) {
-    for (var i = 0; i < labels.length; i++) {
-        if (labels[i][0] == cellAddr) {
-            return (i);
-        }
-    }
-    return (-1);
-}
-
-function updateLabelColors() {
-    workbook.getRangeA1Async("hidden!A2", setHiddenValue, getLabelsStr(0, ","));
-}
-
-function setHiddenValue(asyncResult) {
-    asyncResult.getReturnValue().setValuesAsync([[asyncResult.getUserContext()]], setRangeValues, null);
-}
-
-function setRangeValues(asyncResult) {
-}
-
-function getLabelsStr(index, delimeter) {
-    var labelsList = "";
-    for (var i = 0; i < labels.length; i++) {
-        labelsList += labels[i][index] + delimeter;
-    };
-    return labelsList;
-}
-
-function randomCell() {
-    var r = Math.floor(Math.random() * 30);
-    var c = Math.floor(Math.random() * 15);
-    return {
-        row: r,
-        column: c
-    };
+function resultChanged() {
+    $("#txtOther").val("");
+    ($('#result').val() == "other") ? $("#txtOther").show() : $("#txtOther").hide();
 }
